12e8e218b576cdb85665171b076c383d2730419c
gembancud <gembancud@gmail.com>
2020-01-30 00:37:09 +0800

    made cement-like design changes to cards

--- lib/screens/home/barracksshoplist.dart
+++ lib/screens/home/barracksshoplist.dart
@@ -1,7 +1,9 @@
 import 'package:barracks_app/models/shop.dart';
 import 'package:barracks_app/shared/loading.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:clay_containers/clay_containers.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:map_launcher/map_launcher.dart';
 import 'package:provider/provider.dart';
 import 'package:sticky_headers/sticky_headers.dart';
@@ -25,76 +27,117 @@ class BarracksShopsList extends StatelessWidget {
               },
               content: Column(
                 children: <Widget>[
-                  Card(
-                    elevation: 4.0,
-                    child: Column(
-                      children: <Widget>[
-                        SizedBox(height: 70),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Hero(
-                            tag: 'ShopImageTag',
-                            child: CachedNetworkImage(
-                              imageUrl: _shops[idx].imgUrl,
-                              placeholder: (context, url) =>
-                                  CircularProgressIndicator(),
-                              errorWidget: (context, url, error) =>
-                                  Icon(Icons.error),
-                              fit: BoxFit.cover,
-                            ),
+                  ClayContainer(
+                    color: Colors.grey,
+                    child: Container(
+                      // elevation: 4.0,
+                      child: Container(
+                        padding: EdgeInsets.all(8),
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage(
+                                "assets/img/barracks_background3.png"),
+                            fit: BoxFit.cover,
                           ),
                         ),
-                        ButtonBar(
-                          alignment: MainAxisAlignment.spaceEvenly,
+                        child: Column(
                           children: <Widget>[
-                            OutlineButton(
-                              splashColor: Colors.grey[500],
-                              child: const Text('Book Here',
-                                  style: TextStyle(
-                                      color: Colors.black, fontSize: 14)),
-                              onPressed: () {},
-                            ),
-                            OutlineButton(
-                              splashColor: Colors.grey[500],
-                              child: const Text(
-                                'Get Directions',
-                                style: TextStyle(
-                                    color: Colors.black, fontSize: 14),
+                            SizedBox(height: 70),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 8),
+                              child: Hero(
+                                tag: 'ShopImageTag',
+                                child: CachedNetworkImage(
+                                  imageUrl: _shops[idx].imgUrl,
+                                  placeholder: (context, url) =>
+                                      CircularProgressIndicator(),
+                                  errorWidget: (context, url, error) =>
+                                      Icon(Icons.error),
+                                  fit: BoxFit.cover,
+                                  width: double.infinity,
+                                ),
                               ),
-                              onPressed: () async {
-                                if (await MapLauncher.isMapAvailable(
-                                    MapType.google)) {
-                                  await MapLauncher.launchMap(
-                                    mapType: MapType.google,
-                                    coords: Coords(
-                                        double.parse(_shops[idx].lat),
-                                        double.parse(_shops[idx].long)),
-                                    title: _shops[idx].name,
-                                    description:
-                                        'Barracks Barbers & Shaves Co.',
-                                  );
-                                }
-                              },
                             ),
-                            OutlineButton(
-                              splashColor: Colors.grey[500],
-                              child: const Text(
-                                'Contact Us',
-                                style: TextStyle(
-                                    color: Colors.black, fontSize: 14),
-                              ),
-                              onPressed: () async {
-                                try {
-                                  print('calling ${_shops[idx].phonenumber}');
-                                  launch('tel://${_shops[idx].phonenumber}');
-                                } catch (e) {
-                                  print(e.toString());
-                                }
-                              },
+                            ButtonBar(
+                              alignment: MainAxisAlignment.spaceEvenly,
+                              children: <Widget>[
+                                OutlineButton(
+                                  splashColor: Colors.grey[500],
+                                  child: Row(
+                                    children: <Widget>[
+                                      const Icon(
+                                        FontAwesomeIcons.book,
+                                        color: Colors.white60,
+                                      ),
+                                      const Text(' Book Here',
+                                          style: TextStyle(
+                                              color: Colors.black,
+                                              fontSize: 14)),
+                                    ],
+                                  ),
+                                  onPressed: () {},
+                                ),
+                                OutlineButton(
+                                  splashColor: Colors.grey[500],
+                                  child: Row(
+                                    children: <Widget>[
+                                      const Icon(
+                                        FontAwesomeIcons.mapMarkerAlt,
+                                        color: Colors.white60,
+                                      ),
+                                      const Text(
+                                        ' Find Us',
+                                        style: TextStyle(
+                                            color: Colors.black, fontSize: 14),
+                                      ),
+                                    ],
+                                  ),
+                                  onPressed: () async {
+                                    if (await MapLauncher.isMapAvailable(
+                                        MapType.google)) {
+                                      await MapLauncher.launchMap(
+                                        mapType: MapType.google,
+                                        coords: Coords(
+                                            double.parse(_shops[idx].lat),
+                                            double.parse(_shops[idx].long)),
+                                        title: _shops[idx].name,
+                                        description:
+                                            'Barracks Barbers & Shaves Co.',
+                                      );
+                                    }
+                                  },
+                                ),
+                                OutlineButton(
+                                  splashColor: Colors.grey[500],
+                                  child: Row(
+                                    children: <Widget>[
+                                      const Icon(
+                                        FontAwesomeIcons.phone,
+                                        color: Colors.white60,
+                                      ),
+                                      const Text(
+                                        ' Call Us',
+                                        style: TextStyle(
+                                            color: Colors.black, fontSize: 14),
+                                      ),
+                                    ],
+                                  ),
+                                  onPressed: () async {
+                                    try {
+                                      print(
+                                          'calling ${_shops[idx].phonenumber}');
+                                      launch(
+                                          'tel://${_shops[idx].phonenumber}');
+                                    } catch (e) {
+                                      print(e.toString());
+                                    }
+                                  },
+                                ),
+                              ],
                             ),
                           ],
                         ),
-                      ],
+                      ),
                     ),
                   ),
                   SizedBox(height: 40),
